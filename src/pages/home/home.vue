a8ed77d171179e762f8d337ffb6742c0b06adee4
YourOwen <minyuwen@126.com>
2019-02-05 23:44:26 +0800

    固定的侧边栏

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,7 +1,81 @@
 <template>
+
+<!-- 现在侧边栏是写死了，需要动态获取 -->
+
   <div class="home-container">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <div class="logo"></div>
+         <el-menu
+      default-active="1-1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">
+            <i class="el-icon-menu"></i>用户列表
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">
+            <i class="el-icon-menu"></i>用户列表
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>商品管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">
+            <i class="el-icon-menu"></i>用户列表
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>订单管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="4-1">
+            <i class="el-icon-menu"></i>用户列表
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>数据统计</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="4-1">
+            <i class="el-icon-menu"></i>用户列表
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+
+    </el-menu>
+      </el-aside>
     <el-container>
       <el-header>Header</el-header>
         <el-main>Main</el-main>
@@ -24,6 +98,14 @@ export default {
           type: 'success'
         });
   },
+   methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
 }
 </script>
 
